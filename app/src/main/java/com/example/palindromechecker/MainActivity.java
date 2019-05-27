4e8cfbf5fc5c58ed8854f0e77b19fedcc746b47f
ItsCDubb <chris.warren.2019@gmail.com>
2019-05-27 15:15:30 -0500

    Version 2.75

--- app/src/main/java/com/example/palindromechecker/MainActivity.java
+++ app/src/main/java/com/example/palindromechecker/MainActivity.java
@@ -14,32 +14,31 @@ public class MainActivity extends AppCompatActivity {
     TextView textStart;
     EditText textQuestion;
     TextView textResult;
-    Button buttonSubmit;
+    Button   buttonSubmit;
 
-    boolean isPalindrome;
+    boolean  isPalindrome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textStart = findViewById(R.id.text_start);
+        textStart    = findViewById(R.id.text_start);
         textQuestion = findViewById(R.id.text_question);
         buttonSubmit = findViewById(R.id.button_submit);
-        textResult = findViewById(R.id.text_result);
+        textResult   = findViewById(R.id.text_result);
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("Palindrome", "The Palindrom button was clicked");
-                String s = String.valueOf(textQuestion.getText()).toLowerCase();
-                if (!s.isEmpty()) {
-                    isPalindrome = s.equals(new StringBuilder(s).reverse().toString());
+                Log.i("Palindrome", "The Palindrome button was clicked");
+                    if (!s.isEmpty()) {
+                    isPalindrome = s.equals(new StringBuilder(s).reverse().toString()).toLowerCase;
                     if (isPalindrome == true) {
 
-                        textResult.setText("This is a plaindrome");
+                        textResult.setText("This is a palindrome");
 
                     } else {
-                        textResult.setText("This is not a plaindrome");
+                        textResult.setText("This is not a palindrome");
 
                     }
                 } else {
